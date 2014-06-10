745bea920f733f061a6ac51f627612c281fedd91
Leo Huang <crazysheep2302@gmail.com>
2014-06-10 21:53:08 +1000

    Add move validity and highlighting for all the pieces

--- public/js/chess.js
+++ public/js/chess.js
@@ -1,3 +1,27 @@
+dx = {
+  'NORTH': 0,
+  'EAST': 1,
+  'SOUTH': 0,
+  'WEST': -1,
+  'NORTH-EAST': 1,
+  'SOUTH-EAST': 1,
+  'SOUTH-WEST': -1,
+  'NORTH-WEST': -1
+}
+dy = {
+  'NORTH': -1,
+  'EAST': 0,
+  'SOUTH': 1,
+  'WEST': 0,
+  'NORTH-EAST': -1,
+  'SOUTH-EAST': 1,
+  'SOUTH-WEST': 1,
+  'NORTH-WEST': -1
+}
+var cardinals = ['NORTH', 'EAST', 'SOUTH', 'WEST'];
+var diagonals = ['NORTH-EAST', 'SOUTH-EAST', 'SOUTH-WEST', 'NORTH-WEST'];
+var directions = cardinals.concat(diagonals);
+
 function Board(id) {
   var cells = new Array(8);
   for (var row = 0; row < 8; row++) {
@@ -9,6 +33,7 @@ function Board(id) {
   this.c = document.getElementById(id);
   this.g = this.c.getContext('2d');
   this.clicked = null;
+  this.highlighted = [];
 
   this.draw = function() {
     var tileSize = this.c.width / 8;
@@ -26,21 +51,17 @@ function Board(id) {
       this.g.fillStyle = 'rgba(100, 100, 255, 0.3)';
       this.g.fillRect(x, y, tileSize, tileSize);
     }
-  }
 
-  this.reset = function() {
-    var layout = 'CNBKQBNC';
-    for (var i = 0; i < 8; i++) {
-      cells[0][i].setPiece(layout.charAt(i));
-      cells[7][i].setPiece(layout.charAt(i));
-
-      cells[1][i].setPiece('P');
-      cells[6][i].setPiece('P');
+    for (var i = 0; i < this.highlighted.length; i++) {
+      var x = this.highlighted[i].x * tileSize;
+      var y = this.highlighted[i].y * tileSize;
+      this.g.fillStyle = 'rgba(255, 100, 100, 0.3)';
+      this.g.fillRect(x, y, tileSize, tileSize);
     }
   }
 
   this.click = function(x, y) {
-    console.log('Clicked coordinate', x, y);
+    this.highlighted = [];
 
     var tileSize = c.width / 8;
     var col = Math.floor(x / tileSize);
@@ -48,11 +69,73 @@ function Board(id) {
     cells[row][col].click();
   }
 
+  this.withinBounds = function(pos) {
+    if (pos.x < 0 || pos.x > 7 || pos.y < 0 || pos.y > 7) {
+      return false;
+    }
+    return true;
+  }
+  this.isValid = function(pos) {
+    if (this.withinBounds(pos) && cells[pos.y][pos.x].piece === null) {
+      // Empty and valid
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  this.possibleMoves = function(start, direction, limit) {
+    var pos = {x: start.x + dx[direction], y: start.y + dy[direction]};
+    var moves = [];
+
+    var steps = 0;
+    while (this.isValid(pos)) {
+      moves.push(pos);
+      pos = {x: pos.x + dx[direction], y: pos.y + dy[direction]};
+      steps++;
+
+      if (limit !== undefined && steps === limit) {
+        break;
+      }
+    }
+    if (this.withinBounds(pos)) {
+      if (limit === undefined || steps < limit) {
+        moves.push(pos);
+      }
+    }
+
+    return moves;
+  }
+  this.possibleMovesKnight = function(start) {
+    var knightDX = [-1, -2, -2, -1, 1, 2, 2, 1];
+    var knightDY = [-2, -1, 1, 2, 2, 1, -1, -2];
+    var moves = [];
+
+    for (var i = 0; i < 8; i++) {
+      var pos = {x: start.x + knightDX[i], y: start.y + knightDY[i]};
+      if (this.withinBounds(pos)) {
+        moves.push(pos);
+      }
+    }
+
+    return moves;
+  }
+
+  this.reset = function() {
+    var layout = 'CNBKQBNC';
+    for (var i = 0; i < 8; i++) {
+      cells[0][i].setPiece('BLACK', layout.charAt(i));
+      cells[7][i].setPiece('WHITE', layout.charAt(i));
+
+      cells[1][i].setPiece('BLACK', 'P');
+      cells[6][i].setPiece('WHITE', 'P');
+    }
+  }
   this.reset();
 }
 
 function Cell(board, row, col) {
-  var piece = '';
+  this.piece = null;
   this.board = board;
   this.col = col;
   this.row = row;
@@ -71,18 +154,115 @@ function Cell(board, row, col) {
     g.fillStyle = this.baseColor;
     g.fillRect(x, y, size, size);
 
-    g.fillStyle = '#000000';
-    g.font = size + 'px Arial';
-    g.fillText(piece, x, y + size);
+    if (this.piece !== null) {
+      this.piece.draw(g, size);
+    }
   }
 
-  this.setPiece = function(newPiece) {
-    piece = newPiece;
+  this.setPiece = function(player, pieceType) {
+    this.piece = new Piece(this, player, pieceType);
   }
 
   this.click = function() {
     console.log('    Clicked cell at', this.col, this.row);
-    this.board.clicked = this.board.clicked == this ? null : this;
+
+    if (this.board.clicked === this) {
+      // Unclicking
+      this.board.clicked = null;
+    } else {
+      // Clicking
+      this.board.clicked = this;
+
+      // Check what we have to highlight
+      if (this.piece === null) {
+        return;
+      }
+      if (this.piece.pieceType === 'C') {
+        for (var i = 0; i < cardinals.length; i++) {
+          var moves = this.board.possibleMoves({x: this.col, y: this.row}, cardinals[i]);
+          this.board.highlighted = this.board.highlighted.concat(moves);
+        }
+      } else if (this.piece.pieceType === 'B') {
+        for (var i = 0; i < diagonals.length; i++) {
+          var moves = this.board.possibleMoves({x: this.col, y: this.row}, diagonals[i]);
+          this.board.highlighted = this.board.highlighted.concat(moves);
+        }
+      } else if (this.piece.pieceType === 'N') {
+        var moves = this.board.possibleMovesKnight({x: this.col, y: this.row});
+        this.board.highlighted = this.board.highlighted.concat(moves);
+      } else if (this.piece.pieceType === 'Q') {
+        for (var i = 0; i < directions.length; i++) {
+          var moves = this.board.possibleMoves({x: this.col, y: this.row}, directions[i]);
+          this.board.highlighted = this.board.highlighted.concat(moves);
+        }
+      } else if (this.piece.pieceType === 'K') {
+        for (var i = 0; i < directions.length; i++) {
+          var moves = this.board.possibleMoves({x: this.col, y: this.row}, directions[i], 1);
+          this.board.highlighted = this.board.highlighted.concat(moves);
+        }
+      } else if (this.piece.pieceType === 'P') {
+        var jump = 1;
+        if (this.row === 1 || this.row === 6) {
+          // Starting position
+          jump = 2;
+        }
+
+        var direction;
+        if (this.piece.player === 'BLACK') {
+          direction = 'SOUTH';
+        } else if (this.piece.player === 'WHITE') {
+          direction = 'NORTH';
+        } else {
+          console.log('Piece is neither black nor white - this should never happen');
+        }
+
+        // Moves and initial pos
+        var moves = [];
+        pos = {x: this.col, y: this.row};
+
+        console.log('before diag');
+        // Check diagonal killing
+        var diagPos;
+        diagPos = {x: pos.x + 1, y: pos.y + dy[direction]};
+        if (this.board.withinBounds(diagPos) && !this.board.isValid(diagPos)) {
+          moves.push(diagPos);
+        }
+        diagPos = {x: pos.x - 1, y: pos.y + dy[direction]};
+        if (this.board.withinBounds(diagPos) && !this.board.isValid(diagPos)) {
+          moves.push(diagPos);
+        }
+
+        // Check forward movement
+        for (var i = 0; i < jump; i++) {
+          pos = {x: pos.x, y: pos.y + dy[direction]};
+          if (this.board.isValid(pos)) {
+            moves.push(pos);
+          }
+        }
+        this.board.highlighted = this.board.highlighted.concat(moves);
+      }
+    }
+  }
+}
+
+function Piece(cell, player, pieceType) {
+  this.cell = cell;
+  this.player = player;
+  this.pieceType = pieceType;
+
+  this.draw = function(g, size) {
+    if (this.player === 'BLACK') {
+      g.fillStyle = '#000000';
+    } else if (this.player === 'WHITE') {
+      g.fillStyle = '#eeeeee';
+    } else {
+      console.log('drawing piece with no player', this.player);
+    }
+    g.font = size + 'px Arial';
+
+    var x = this.cell.col * size;
+    var y = this.cell.row * size;
+    g.fillText(this.pieceType, x, y + size);
   }
 }
 
